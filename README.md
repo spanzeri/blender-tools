6ee58cedd0ded0d2c12b18be7a9c3797f4ac75a4
Samuele Panzeri <samuele.panzeri@gmail.com>
2023-05-01 21:45:01 +0200

    Tool to create TGT bones (and constraint) from deformation bones on an armature. Inspired by the rigging course from p2design

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# blender_tools
-A collection of awful tools I quickly put together for personal use
+# sams-blender-tools
+
+A collection of awful tools I quickly put together for personal use.
+
+None of those are particularly good or generic, this is just a place where I might or might not add addons I write from time to time.
+
+**Use at your personal risk**
---
+++ adddon_make_tgs_bones.py
@@ -0,0 +1,158 @@
+bl_info = {
+    "name": "Sam's Rigging Helpers",
+    "author": "Samuele Panzeri",
+    "version": (1, 0, 0),
+    "blender": (3, 0, 0),
+    "location": "View3d > Tool",
+    "warning": "Use at your own risk",
+    "wiki_url": "",
+    "category": "Rigging"
+}
+
+import bpy
+
+class SRT_PT_RiggingHelperPanel(bpy.types.Panel):
+    bl_label = "Rigging Helpers"
+    bl_idname = "SRT_PT_RiggingHelper"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_category = "Tool"
+    
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        if context.mode != "EDIT_ARMATURE":
+            row.label(text = "Not in armature edit mode", icon = "QUESTION")
+            return
+        
+        row.label(text = "Sample Text", icon = "TRACKER")
+        row = layout.row()
+        row.operator("armature.create_target_bones")
+      
+      
+def dump(data):
+    for attr in dir(data):
+        print("%s = %r" % (attr, getattr(data, attr)))
+
+
+def make_tgt_name(bone_name):
+    if bone_name.upper().startswith("DEF-"):
+        bone_name = bone_name[4:]
+    return "TGT-" + bone_name
+
+
+bone_readonly_attr = [
+    "__doc__", "__module__", "__slots__",
+    "align_orientation", "align_roll",
+    "bl_rna", "children", "rna_type",
+    "transform", "translate",
+    "bl_rna_get_subclass", "bl_rna_get_subclass_py"]
+
+
+class SRT_OT_CreateTargetBone(bpy.types.Operator):
+    """ Create a target bone, which is a non-deform bone with the same name prefixed by TGT and add a copy transform constraint
+    from the original to the newly create target"""
+    bl_idname = "armature.create_target_bones"
+    bl_label = "Create TGT bones"
+    
+    def execute(self, context):
+        if context.mode != "EDIT_ARMATURE":
+            self.report({"ERROR"}, "Not in armature edit mode")
+            return {"CANCELLED"}
+        
+        # Remove existing selection
+        bpy.ops.armature.select_all(action = "DESELECT")
+                
+        armature = context.object
+        if armature.type != "ARMATURE":
+            return {"CANCELLED"}
+                
+        armature_data = armature.data
+        pose = armature.pose
+        if not armature_data or not pose:
+            return {"CANCELLED"}
+        
+        root_names = ["ROOT", "Root", "root"]
+        for rname in root_names:
+            root = armature_data.edit_bones.get(rname)
+            if root:
+                break
+        if not root:
+            self.report({"ERROR"}, "Need a root bone for the armature called ROOT")
+            return {"CANCELLED"}
+        
+        new_bones = []
+        constraint_map = []
+        layers = [False] * 32
+        layers[31] = True
+        
+        # Make duplicate bones with the correct name
+        for bone in armature_data.edit_bones:
+            if not bone.use_deform or bone.name.upper().startswith("TGT-"):
+                continue
+            tgt_bone_name = make_tgt_name(bone.name)
+            print(tgt_bone_name)
+            
+            if armature_data.edit_bones.get(tgt_bone_name):
+                # Bone already exists
+                continue
+            
+            tgt_bone = armature_data.edit_bones.new(tgt_bone_name)
+            for attr in dir(tgt_bone):
+                if attr.find("") != -1 and attr not in bone_readonly_attr:
+                    if attr == "name" or attr == "layers":
+                        pass
+                    else:
+                        setattr(tgt_bone, attr, getattr(bone, attr))
+            
+            tgt_bone.use_connect = False
+            tgt_bone.layers = layers
+            
+            new_bones.append(tgt_bone)
+            constraint_map.append({"name": bone.name, "target": tgt_bone_name})
+        
+        # Re-create the hyerarchy on the target bones
+        for bone in new_bones:
+            if not bone.parent or bone.parent == root:
+                continue
+            
+            parent_name = make_tgt_name(bone.parent.name)
+            new_parent = armature_data.edit_bones.get(parent_name)
+            if not new_parent:
+                new_parent = root
+            
+            bone.parent = new_parent
+            
+        # Switch to pose mode to create constraints
+        bpy.ops.object.mode_set(mode='POSE')
+        for item in constraint_map:
+            cst = pose.bones[item["name"]].constraints.new("COPY_TRANSFORMS")
+            cst.target = armature
+            cst.subtarget = item["target"]
+            
+        return  {"FINISHED"}
+    
+    
+    def invoke(self, context, _event):
+        return self.execute(context)
+        
+        
+def menu_func(self, context):
+    self.layout.operator(SRT_OT_CreateTargetBone.bl_idname)
+
+        
+def register():
+    bpy.utils.register_class(SRT_PT_RiggingHelperPanel)
+    bpy.utils.register_class(SRT_OT_CreateTargetBone)
+    bpy.types.VIEW3D_MT_edit_armature.append(menu_func)
+        
+        
+def unregister():
+    bpy.utils.unregister_class(SRT_PT_RiggingHelperPanel)
+    bpy.utils.unregister_class(SRT_OT_CreateTargetBone)
+    bpy.types.VIEW3D_MT_edit_armature.remove(menu_func)
+    
+    
+if __name__ == "__main__":
+    register()
+    
